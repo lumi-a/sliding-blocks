516c91da999565611ddac09ca5eecc95881e229a
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-06-16 16:45:31 +0200

    Example puzzle to solve

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+pub fn solve_puzzle(start: &str, end: &str) {}
+
+fn main() {
+    let puzzle = (
+        "
+    #t#CCvv
+    A.#DFFv
+    #.#DDrr
+    #.BEEuu
+    #s#E
+    ",
+        "
+    #.#D
+    B.#DD
+    #.#CC
+    #.AEE
+    #.#EFF
+    ",
+    );
+    println!("Hello, world!");
+}
