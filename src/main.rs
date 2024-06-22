fb53a06817624b6537040e97451a3c767fde06fb
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-06-22 14:49:04 +0200

    Revert to string_to_charcoorsmap

--- src/main.rs
+++ src/main.rs
@@ -19,69 +19,60 @@ struct Blockstate(Vec<Offsets>);
 
 const BOUNDS_CHAR: char = '.';
 
-fn string_to_offsetshapes(s: &str) -> Vec<(Offset, Shape)> {
-    let mut global_min_x = Coor::MAX;
-    let mut global_min_y = Coor::MAX;
+fn string_to_charcoorsmap(s: &str) -> HashMap<char, CoordinatesSet> {
+    // TODO: Rename the function, and variables that later call this function
+    // (the current name is horrible)
 
-    let mut temp_coords: HashMap<char, (Coor, Coor, CoordinatesSet)> = HashMap::new();
-    fn insert_temp_coords(
-        c: char,
-        x: Coor,
-        y: Coor,
-        temp_coords: &mut HashMap<char, (Coor, Coor, CoordinatesSet)>,
-    ) {
-        let existing =
-            temp_coords
-                .entry(c)
-                .or_insert((Coor::MAX, Coor::MAX, CoordinatesSet::new()));
-        existing.0 = min(existing.0, x);
-        existing.1 = min(existing.1, y);
-        existing.2.insert(Coordinates(x, y));
-    }
+    let mut min_x = Coor::MAX;
+    let mut min_y = Coor::MAX;
+
+    let mut temp_coords = Vec::new();
 
     for (y, l) in s.lines().enumerate() {
         for (x, c) in l.chars().enumerate() {
             if !c.is_whitespace() {
-                // TODO: Check that x and y fit into the CoorComponent type.
-                // Doing so would mean we'd have to return a Result instead.
-                // Currently, this doesn't even panic, but continues innocently (yet wrongly)
-                // TODO: Then also let x, y be usize here, and only later convert
-                // them to Coor after subtraction
                 let x = x as Coor;
                 let y = y as Coor;
-                global_min_x = min(global_min_x, x);
-                global_min_y = min(global_min_y, y);
-
-                insert_temp_coords(c, x, y, &mut temp_coords);
+                // TODO: Check that x and y fit into the Coor type.
+                // Doing so would mean we'd have to return a Result instead.
+                // Currently, this doesn't even panic, but continues innocently (yet wrongly)
+                min_x = min(min_x, x);
+                min_y = min(min_y, y);
+                temp_coords.push((c, Coordinates(x, y)));
                 if c != BOUNDS_CHAR {
-                    insert_temp_coords(BOUNDS_CHAR, x, y, &mut temp_coords);
+                    temp_coords.push((BOUNDS_CHAR, Coordinates(x, y)));
                 }
             }
         }
     }
 
-    temp_coords
-        .into_values()
-        .map(|(min_x, min_y, coors)| {
-            (
-                Offset(min_x - global_min_x, min_y - global_min_y),
-                Shape(
-                    coors
-                        .into_iter()
-                        .map(|Coordinates(x, y)| Coordinates(x - min_x, y - min_y))
-                        .collect(),
-                ),
-            )
-        })
-        .collect()
+    let shift = Coordinates(min_x, min_y);
+    let mut charmap: HashMap<char, CoordinatesSet> = HashMap::new();
+    for (c, coor) in temp_coords {
+        let shifted_coor = Coordinates(coor.0 - shift.0, coor.1 - shift.1);
+        charmap
+            .entry(c)
+            .or_insert_with(CoordinatesSet::new)
+            .insert(shifted_coor);
+    }
+
+    charmap
 }
 
 pub fn solve_puzzle(start: &str, goal: &str) {
-    let start_offsetshapes = string_to_offsetshapes(start);
-    let goal_offsetshapes = string_to_offsetshapes(goal);
-    // TODO: Maybe check that start and goal have the same bounds?
+    let start_charcoorsmap = string_to_charcoorsmap(start);
+    let goal_charcoorsmap = string_to_charcoorsmap(goal);
 
-    println!("{start_offsetshapes:?} {goal_offsetshapes:?}");
+    // TODO: Handle this gracefully rather than panicking
+    assert_eq!(
+        start_charcoorsmap
+            .get(&BOUNDS_CHAR)
+            .unwrap_or(&CoordinatesSet::new()),
+        goal_charcoorsmap
+            .get(&BOUNDS_CHAR)
+            .unwrap_or(&CoordinatesSet::new()),
+        "The start and goal must have the same bounds."
+    );
 }
 
 fn main() {
