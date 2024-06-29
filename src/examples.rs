c76767ddad1bcfca13efa0cae2861e2eb0a5b22a
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-06-29 19:46:44 +0200

    Heyyyy new examples

--- src/examples.rs
+++ src/examples.rs
@@ -1,9 +1,90 @@
 pub struct Puzzle {
+    pub name: &'static str,
     pub start: &'static str,
     pub goal: &'static str,
+    pub moves: usize,
 }
 
+pub const ALL_EXAMPLES: [Puzzle; 6] = [
+    TEN_STEP_SOLUTION,
+    FOUR_BALLS,
+    THE_ELEVATOR_SWITCH,
+    GARBAGE_DISPOSAL,
+    THE_DIABOLICAL_BOX,
+    ROYAL_ESCAPE,
+];
+
+pub const TEN_STEP_SOLUTION: Puzzle = Puzzle {
+    name: "Ten-Step Solution",
+    start: r#"  
+      A  
+    11.  
+    .22. 
+    .33. 
+     .44 
+     B   
+    "#,
+    goal: r#"  
+      B  
+    ...  
+    .... 
+    .... 
+     ... 
+     A 
+    "#,
+    moves: 10,
+};
+
+pub const FOUR_BALLS: Puzzle = Puzzle {
+    name: "Four Balls",
+    start: r#"
+        A
+    B|.--
+     |##.
+     .##I
+     ==.ID
+     C
+    "#,
+    goal: r#"
+        C
+    D....
+     ....
+     ....
+     ....B
+     A
+    "#,
+    moves: 28,
+};
+
+pub const THE_ELEVATOR_SWITCH: Puzzle = Puzzle {
+    name: "The Elevator Switch",
+    start: r#"
+       AA   
+       AA   
+    ...a....
+    ##aabb..
+    ## cb XX
+    ..ccddXX
+    ....d...
+       BB
+       BB
+    "#,
+    goal: r#"
+       BB   
+       BB   
+    ........
+    ........
+    .. .. ..
+    ........
+    ........
+       AA
+       AA
+    "#,
+    moves: 12,
+};
+
 pub const GARBAGE_DISPOSAL: Puzzle = Puzzle {
+    name: "Garbage Disposal",
     start: r#"
       tt
       tt
@@ -24,9 +105,11 @@ pub const GARBAGE_DISPOSAL: Puzzle = Puzzle {
       tt
       tt
     "#,
+    moves: 31,
 };
 
-pub const DIABOLICAL_BOX: Puzzle = Puzzle {
+pub const THE_DIABOLICAL_BOX: Puzzle = Puzzle {
+    name: "The Diabolical Box",
     start: r#"
       ##    
       ##    
@@ -49,9 +132,11 @@ pub const DIABOLICAL_BOX: Puzzle = Puzzle {
       ##    
       ##
     "#,
+    moves: 78,
 };
 
 pub const ROYAL_ESCAPE: Puzzle = Puzzle {
+    name: "Royal Escape",
     start: r#"
     --==+
     ##|/.
@@ -64,4 +149,5 @@ pub const ROYAL_ESCAPE: Puzzle = Puzzle {
     ...##
     .....
     "#,
+    moves: 81,
 };
