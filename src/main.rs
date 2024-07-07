a311e57c0b982fb9c48e2b0802385b6e1e89ae8e
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-07-07 16:51:33 +0200

    Add path reconstruction, change return type of solve_puzzle

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use sliding_blocks::{examples, solve_puzzle};
 
 fn main() {
     for puzzle in examples::ALL_EXAMPLES {
-        let computed = solve_puzzle(puzzle.start, puzzle.goal);
+        let computed = solve_puzzle(puzzle.start, puzzle.goal).len() - 1;
         let solution = puzzle.moves;
         println!(
             "{} {}{}{}",
